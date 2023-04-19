fc6cf7ab6403ef248c1db88c8f3def3670f3afb3
Filipe Santos <125661651+1fox4@users.noreply.github.com>
2023-04-19 15:52:25 +0100

    noncanonical

---
+++ noncanonical.c
@@ -0,0 +1,103 @@
+cona
+Filipe Berenguel Amaro dos Santos
+qua, 19/04/2023 15:50
+https://github.com/1fox4/RCOM-LAB3.git
+
+Traduzir a mensagem para: Português (Brasil)
+| Nunca traduzir do: Inglês
+Guilherme Manuel Carneiro Pereira
+Filipe Berenguel Amaro dos Santos
+/*Non-Canonical Input Processing*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+
+volatile int STOP=FALSE;
+
+int main(int argc, char** argv)
+{
+    int fd,c, res;
+    struct termios oldtio,newtio;
+    char buf[255];
+
+    if ( (argc < 2) ||
+         ((strcmp("/dev/ttyS0", argv[1])!=0) &&
+          (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+        printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+        exit(1);
+    }
+
+
+    /*
+    Open serial port device for reading and writing and not as controlling tty
+    because we don't want to get killed if linenoise sends CTRL-C.
+    */
+
+
+    fd = open(argv[1], O_RDWR | O_NOCTTY );
+    if (fd < 0) { perror(argv[1]); exit(-1); }
+
+    if (tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    /* set input mode (non-canonical, no echo,...) */
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+
+    /*
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+    leitura do(s) próximo(s) caracter(es)
+    */
+
+
+    tcflush(fd, TCIOFLUSH);
+
+    if (tcsetattr(fd,TCSANOW,&newtio) == -1) {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("New termios structure set\n");
+    char aux[255];
+    int i=0;
+    while (STOP==FALSE) {       /* loop for input */
+        res = read(fd,buf,1);   /* returns after 5 chars have been input */
+        buf[res]=0;               /* so we can printf... */
+        aux[i] = buf[0];
+        i++;
+       
+        if (buf[0]=='\0'){
+        printf("%s\n", aux);
+        STOP=TRUE;
+        }
+    }
+
+    res = write(fd,aux,strlen(aux)+1);
+    printf("%d bytes written\n", res);
+
+    /*
+    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no guião
+    */
+
+    tcsetattr(fd,TCSANOW,&oldtio);
+    close(fd);
+    return 0;
+}
+
